eaba9827878f532debf15b17f1b5a911fb8c81f0
Cheegz <bentleybb@gmail.com>
2017-09-08 23:05:36 -0600

    add 03_variables code

---
+++ 05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"log"
+)
+func main() {
+	res, err := http.Get("http://www.google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err!= nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", page)
+}
